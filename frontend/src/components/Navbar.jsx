df8b0c2920e9bf190463ad6091297f86a5955a1e
Eddy G <eddy.spambari@gmail.com>
2023-08-01 23:59:55 -0700

    navbar styling is now in seperate css file

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,30 +1,51 @@
 /** @format */
+
 import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import React from "react";
+import "./styles/Navbar.css";
+import "animate.css";
 
 function NavbarTop({ connect, connected }) {
    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-         <Container fluid>
-            <Navbar.Brand href="/">NFT Prize Pool</Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse>
-               <Nav className="me-auto">
-                  <Nav.Link href="/submit-nft">Submit NFT</Nav.Link>
-                  <Nav.Link href="/votes">Vote</Nav.Link>
-               </Nav>
-               <Nav>
-                  {!connected ? (
-                     <Button onClick={connect}>Connect to Metamask</Button>
-                  ) : (
-                     <p style={{ color: "white" }}>Connected to Metamask.</p>
-                  )}
-               </Nav>
-            </Navbar.Collapse>
-         </Container>
-      </Navbar>
+      <>
+         <Navbar
+            collapseOnSelect
+            expand="lg"
+            className="navbar-container"
+            variant="dark"
+         >
+            <Container fluid>
+               <Navbar.Brand href="/" className="navbar-brand ">
+                  <span className="wave">NFT Prize Pool</span>
+               </Navbar.Brand>
+               <Navbar.Toggle />
+               <Navbar.Collapse>
+                  <Nav className="me-auto">
+                     <Nav.Link href="/submit-nft" className="nav-link">
+                        Submit NFT
+                     </Nav.Link>
+                     <Nav.Link href="/votes" className="nav-link">
+                        Vote
+                     </Nav.Link>
+                  </Nav>
+                  <Nav>
+                     {!connected ? (
+                        <Button onClick={connect} className="connect-button">
+                           Connect to Metamask
+                        </Button>
+                     ) : (
+                        <p className="connected-message">
+                           Connected to Metamask
+                        </p>
+                     )}
+                  </Nav>
+               </Navbar.Collapse>
+            </Container>
+         </Navbar>
+      </>
    );
 }
 
