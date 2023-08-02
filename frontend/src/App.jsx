afe86fe8d4a173073c7875e25ad802357056f597
Eddy G <eddy.spambari@gmail.com>
2023-08-01 23:59:21 -0700

    added basic functions for contract/connecting, Navbar displayer, paths to different components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,55 @@
 /** @format */
-import "./styles/App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Submit from "./components/Submit.jsx";
+import Votes from "./components/Votes.jsx";
+
+import { useState, useEffect } from "react";
+import { connect, getContract } from "./components/SmartContract.jsx";
 
 function App() {
-   return <></>;
+   const [contract, setContract] = useState(null);
+   const [connected, setConnected] = useState(false);
+
+   useEffect(() => {
+      window.ethereum.request({ method: "eth_accounts" }).then((accounts) => {
+         if (accounts.length > 0) {
+            handleInit();
+         } else setConnected(false);
+      });
+   }, []);
+
+   const handleInit = () => {
+      setConnected(true);
+      getContract().then(({ contract, signer }) => {
+         setContract(contract);
+      });
+   };
+
+   const connectCallback = async () => {
+      const { contract } = await connect();
+      setContract(contract);
+      if (contract) {
+         setConnected(true);
+      }
+   };
+
+   return (
+      <Router>
+         <Navbar connect={connectCallback} connected={connected} />
+
+         <div className="container">
+            <Routes>
+               <Route
+                  path="submit-nft"
+                  element={<Submit contract={contract} />}
+               />
+               <Route path="votes" element={<Votes contract={contract} />} />
+            </Routes>
+         </div>
+      </Router>
+   );
 }
 
 export default App;
