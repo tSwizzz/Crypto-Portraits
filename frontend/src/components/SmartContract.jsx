45f8560dc91db3db1d017b0607284736565cb396
Eddy G <eddy.spambari@gmail.com>
2023-08-03 07:26:39 -0700

    basic UI atm for testing things out

--- frontend/src/components/SmartContract.jsx
+++ frontend/src/components/SmartContract.jsx
@@ -1,11 +1,11 @@
 /** @format */
 import { ethers } from "ethers";
-
-let contract;
-let signer;
 const provider = new ethers.BrowserProvider(window.ethereum);
-const address = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
-const abi = [
+let signer;
+
+let prizePoolContract;
+const prizePoolAddress = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
+const prizePoolAbi = [
    "constructor() nonpayable",
    "function allParticipants(uint256) view returns (address)",
    "function beginContestValue() view returns (bool)",
@@ -22,19 +22,84 @@ const abi = [
    "function withdrawPrizePool()",
 ];
 
+let nftContract;
+const nftAddress = "0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e";
+const nftAbi = [
+   "constructor() nonpayable",
+   "event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId)",
+   "event ApprovalForAll(address indexed owner, address indexed operator, bool approved)",
+   "event BatchMetadataUpdate(uint256 _fromTokenId, uint256 _toTokenId)",
+   "event MetadataUpdate(uint256 _tokenId)",
+   "event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)",
+   "event Transfer(address indexed from, address indexed to, uint256 indexed tokenId)",
+   "function approve(address to, uint256 tokenId)",
+   "function balanceOf(address owner) view returns (uint256)",
+   "function getApproved(uint256 tokenId) view returns (address)",
+   "function isApprovedForAll(address owner, address operator) view returns (bool)",
+   "function mint(address _to, uint256 _tokenId, string _uri)",
+   "function name() view returns (string)",
+   "function owner() view returns (address)",
+   "function ownerOf(uint256 tokenId) view returns (address)",
+   "function renounceOwnership()",
+   "function safeTransferFrom(address from, address to, uint256 tokenId)",
+   "function safeTransferFrom(address from, address to, uint256 tokenId, bytes data)",
+   "function setApprovalForAll(address operator, bool approved)",
+   "function supportsInterface(bytes4 interfaceId) view returns (bool)",
+   "function symbol() view returns (string)",
+   "function tokenURI(uint256 tokenId) view returns (string)",
+   "function transferFrom(address from, address to, uint256 tokenId)",
+   "function transferOwnership(address newOwner)",
+];
+
 export const connect = async () => {
    await provider.send("eth_requestAccounts", []);
-   return getContract();
+   return getAccess();
 };
 
-export const getContract = async () => {
+export const getAccess = async () => {
    signer = await provider.getSigner();
-   contract = new ethers.Contract(address, abi, signer);
-   return { signer: signer, contract: contract };
+   prizePoolContract = new ethers.Contract(
+      prizePoolAddress,
+      prizePoolAbi,
+      signer,
+   );
+   nftContract = new ethers.Contract(nftAddress, nftAbi, signer);
+
+   return { signer: signer, contract: prizePoolContract };
 };
 
-export const getContestStatus = async () => {
-   await getContract();
-   const value = contract.beginContestValue();
-   return value;
-};
+export async function approveNFT() {
+   await getAccess();
+   const id = document.getElementById("token-id-approve").value;
+
+   await nftContract
+      .approve(prizePoolAddress, id)
+      .then(() => alert("Your NFT is now approved!"))
+      .catch((error) => {
+         if (error.data) alert(error.data.message);
+         else alert(error);
+      });
+}
+
+export async function submit() {
+   await getAccess();
+   const id = document.getElementById("token-id").value;
+   const amount = document.getElementById("buy-in-amount").value;
+
+   await prizePoolContract
+      .submitNFT(nftAddress, id, { value: amount })
+      .then(() => {
+         alert("Your NFT has been submitted to the contest! Stay tuned!");
+      })
+      .catch((error) => {
+         if (error.data.message) {
+            alert(error.data.message);
+         } else {
+            alert(error);
+         }
+      });
+
+   //if ((await nftContract.balanceOf(prizePoolAddress)) == 3) {
+   //   //begins contest
+   //}
+}
--- frontend/src/components/Submit.jsx
+++ frontend/src/components/Submit.jsx
@@ -1,8 +1,43 @@
 /** @format */
-import React from "react";
 
-function Submit() {
-   return <div>Submit Component</div>;
+import React, { useState } from "react";
+import "./styles/Submit.css";
+import { approveNFT, submit } from "./SmartContract.jsx";
+
+function Submit({ contract }) {
+   const [submitNum, setSubmitNum] = useState(0);
+
+   const handleApproveNFT = async () => {
+      approveNFT();
+   };
+
+   const handleSubmitNFT = async () => {
+      submit();
+   };
+
+   return (
+      <>
+         <div>
+            <input
+               type="number"
+               id="token-id-approve"
+               placeholder="Token ID"
+               min="0"
+            />
+            <button onClick={handleApproveNFT}>Approve NFT</button>
+         </div>
+         <div>
+            <input type="number" id="token-id" placeholder="Token ID" min="1" />
+            <input
+               type="number"
+               id="buy-in-amount"
+               placeholder="1000 wei to submit NFT"
+               min="0"
+            />
+            <button onClick={handleSubmitNFT}>Submit NFT</button>
+         </div>
+      </>
+   );
 }
 
 export default Submit;
